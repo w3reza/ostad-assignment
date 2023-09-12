3125e0616d03e34ecb5e447de69748928b562182
w3reza <w3reza@gmail.com>
2023-09-12 14:59:02 +0600

    01- assignment

--- personal_info.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Create a PHP file named personal_info.php that displays your personal information using variables and the echo statement. Include your name, age, country, and a brief introduction.
-$name = "Md. Rezaul Alam";
-$age = 33;
-$country = "Bangladesh";
-$introduction = " I'm from Pirojpur but currently live in Svar. I have completed my graduation of Bsc in CSE (Honours) in the year 2015 from BUBT. Now I am working in a public University of Bangladesh.";
-echo "I am ".$name.", ".$age." years old, I live in ".$country.".".$introduction;
-?>
